4e0f677a36fc5aa35f7750c17e7bcf69782e5a29
czf0122 <chenzhaofeng0122@163.com>
2018-05-12 15:46:12 +0800

    IDEA整合Git1 新增语句上传GitHub

--- src/main/Java/com/czf/HelloIdea.java
+++ src/main/Java/com/czf/HelloIdea.java
@@ -3,5 +3,10 @@ package com.czf;
 public class HelloIdea {
 	public static void main(String[] args) {
 		System.out.println("Heloo,Idea");
+		System.out.println("Heloo,Idea");
+		System.out.println("Heloo,Idea");
+		System.out.println("Heloo,Idea");
+		System.out.println("Heloo,Idea");
+
 	}
 }
